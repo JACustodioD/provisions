b0b2b53c2363dc5933def8b98e10b576d3bd58bb
JACDC <jacustodiod@hotmail.com>
2019-09-02 18:30:05 -0500

    inital commit

---
+++ resources/views/Bodas/juliaPablo/index.blade.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Invitación</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+	<meta property="og:image" content="/img/bodas/carta.jpeg">
+	<meta property="og:title" content="Julia & Pablo" />
+  	<meta property="og:url" content="provisionssystems.com/invitations/JuliaAndPablo" />
+  	<meta property="og:description" content="Acompañanos a celebrar nuestra boda. Te esperamos...">
+	    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Tangerine&display=swap" rel="stylesheet"> 
+
+ 	<link rel="stylesheet" type="text/css" href="/css/index.css">
+</head>
+<body data-spy="scroll" data-target="#navbar" data-offset="57">
+	
+	<!-- Navegacion -->
+	<nav id="header" class="navbar navbar-expand-lg navbar-dark sticky-top " >
+  		<!--<a class="navbar-brand" href="#">Navbar</a>-->
+  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    		<i class="fas fa-ring"></i>
+ 		 </button>
+
+  		<div class="collapse navbar-collapse" id="navbar">
+    		<ul class="navbar-nav ml-auto">
+		      <li class="nav-item mr-4">
+		        <a class="nav-link " href="#history">
+		        	<i class="fas fa-history"></i>
+		        	Historia
+		        	<span class="sr-only">(current)</span>
+		        </a>
+		      </li>
+		      <li class="nav-item mr-4">
+		        <a class="nav-link" href="#ubication">
+					<i class="fas fa-street-view"></i>
+		        	Ubicación
+		        </a>
+		      </li>
+		      <li class="nav-item mr-4">
+		        <a class="nav-link" href="#gallery">
+		        	<i class="far fa-images"></i>
+		        	Galeria de Fotos
+		    	</a>
+		      </li>
+		      <li class="nav-item mr-4">
+		        <a class="nav-link" href="#gifts">
+		        	<i class="fas fa-gifts"></i>
+		        	Mesa de Regalos
+		        </a>
+		      </li>
+		  	</ul>
+		</div>
+	</nav>
+	<!-- /Navegacion -->
+
+
+	<!--Main-->
+	<main id="main">
+		<div id="carousel" class="carousel slide carousel-fade" data-ride="carousel" data-pause="false">
+  			<div class="carousel-inner">
+	    		<div class="carousel-item active">
+	      			<img src="/img/bodas/slider3.jpg" class="d-block w-100" alt="...">
+	    		</div>
+			    <div class="carousel-item">
+			      <img src="/img/bodas/slider2.png" class="d-block w-100" alt="...">
+			    </div>
+			    <div class="carousel-item">
+			      <img src="/img/bodas/slider1.jpg" class="d-block w-100" alt="...">
+			    </div>
+  			</div>
+  			<div class="overlay text-center">
+				<div class="container tituloInv">
+					<div class="row d-flex justify-content-center align-items-center">
+						<div class="col-12">
+							<h1>Te invitamos a nuestra boda</h1>
+							<h2>Julia Apellido Apellido <br> And <br> Pablo Apelldio Apellido</h2>
+							<h3>Te esperamos...</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+	<!--/Main -->
+
+	<!-- History-->
+	<section id="history" class="text-center">
+		<div class="container-fluid ">
+			<div class="row">
+				<div class="col-6 col-md-3">
+					<h2>Nuestra Historia</h2>
+				</div>
+				<div class="col-6 col-md-3 offset-md-6">
+					<h2>Faltan: 40 días</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-8 offset-2  text-justify">
+					<p id="textHistory">
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-12">
+					<img src="/img/bodas/anillos.png" height="150">
+				</div>
+			</div>
+		</div>
+		<hr>
+	</section>
+	<!-- /History-->
+
+	<!-- Location-->
+	<section>
+		<div class="container" id="ubication">
+			<div class="row pb-4">
+				<div class="col text-center">
+					<h1 id="titleUbication">Ubicación de la boda</h1>
+				</div>
+			</div>
+			<div class="row text-center">
+				<div class="col-12 col-lg-6">
+					<h2>Ubicación de la boda</h2>
+					<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					</p>
+					<img class="ubicationImg" src="/img/bodas/ubicacion.jpeg" height="150">
+				</div>
+				<div class="col-12 col-lg-6">
+					<h2>Ubicación de la fiesta</h2>
+					<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					</p>
+					<img class="ubicationImg" src="/img/bodas/ubicacion.jpeg" height="150">
+				</div>
+			</div>
+		</div>
+		<br>
+	</section>
+	<!-- /Location-->
+
+	<!-- Galery -->
+	<section id="gallery">
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+			    	<img src="/img/bodas/slider3.jpg" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+				    <img src="/img/bodas/slider3.jpg" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+				    <img src="/img/bodas/slider3.jpg" class="d-block w-100" alt="...">
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+			    <span class="sr-only">Next</span>
+			</a>
+		</div>
+	</section>
+	<!-- /Galery-->
+
+	<!--Footer -->
+	<footer id="footer">
+		<div class="container ">
+			<div class="row">
+				<div class="col-12 col-md-4">
+					<label>Invitaciones:</label>
+					<ul>
+						<li>
+							<a href="#">
+								<i class="fab fa-facebook"></i>
+								facebook.com/ProVisionsSystems
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="fas fa-globe-americas"></i>
+								provisionssystems.com
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="fab fa-twitter-square"></i>
+								twitter.com/ProVisionsSyst
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-12 col-md-4 offset-md-4">
+					<label>Fotografías:</label>
+					<ul>
+						<li>
+							<a href="#">
+								<i class="fab fa-facebook"></i>
+								facebook.com/StudioPhotos
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="fab fa-instagram"></i>
+								instragram.com/StudioPhotos
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<i class="fab fa-twitter-square"></i>
+								twitter.com/StudioPhotos
+							</a>
+						</li>
+					</ul>
+				</div>	 
+			</div>
+		</div>
+	</footer>
+	<!-- /Footer -->
+	
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	
+	<!-- Icons -->
+	<script type="text/javascript" src="/js/all.js"></script>
+</body>
+</html>
