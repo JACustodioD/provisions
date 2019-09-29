79c387d656f9bd53ab7b3b8018940ba76211cd58
JACustodioD <JACustodioD@hotmail.com>
2019-09-28 23:38:30 -0500

    agragando header a pvsDinner

--- resources/views/templetes/laborales/provisions.blade.php
+++ resources/views/templetes/laborales/provisions.blade.php
@@ -2,11 +2,20 @@
 <html lang="es">
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
+    <meta property="og:image" content="/templetes/img/bodas/carta.jpeg">
+    <meta property="og:title" content="Cena de Navidad" />
+    <meta property="og:url" content="provisionssystems.com/invitations/provisionsDinner" />
+    <meta property="og:description" content="Porque la Navidad se trata de celebrar juntos.  Acompañanos a la cena navideña de tu empresa">
+    <!-- FONTS -->
+     <link href="https://fonts.googleapis.com/css?family=Dosis|Nanum+Gothic|Great+Vibes|News+Cycle&display=swap" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet"> 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Css -->
+    <link rel="stylesheet" type="text/css" href="/templetes/css/pvsDinner.css">
 
     <title>Fiesta de navidad</title>
   </head>
@@ -14,12 +23,63 @@
     <nav id="nevegador">
       
     </nav>
-    <main id="main">
-      <div class="container">
-        <h1 class="alert-info">Navidad presente en ProVisions Systems</h1>
-      </div>
-    </main>
     
+  <!--Main-->
+  <main id="main">
+    <div id="carousel" class="carousel slide carousel-fade" data-ride="carousel" data-pause="false">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+              <img src="/templetes/img/reuniones/portada.jpeg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="/templetes/img/reuniones/portada2.jpg" class="d-block w-100" alt="slider 2">
+          </div>
+          <div class="carousel-item">
+            <img src="/templetes/img/reuniones/portada3.jpg" class="d-block w-100" alt="slider 3">
+          </div>
+        </div>
+        <div class="overlay text-center">
+        <div class="container tituloInv">
+          <div class="row d-flex justify-content-center align-items-center">
+            <div class="col-12">
+              <h1>¡Feliz Navidad!</h1>
+              <h2>Porque la navidad es para compartir en familia</h2>
+              <h3>Te esperamos...</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  <!--/Main -->
+  <!-- -->
+  <section id="textInitial" class="mt-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-6 mt-4">
+          <img id="logo" src="/img/PVS1.png" width="600">
+        </div>
+        <div class="col-12 col-md-6">
+          <h1>ProVisions Systems </h1>
+          <p class="text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /-->
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
