70c8b2ec8b9c68e988d8699a1dd413c0bd154b2a
JACustodioD <jacustodiod@hotmail.com>
2019-09-30 15:55:54 -0500

    footer en invitacion cena

--- resources/views/templetes/laborales/provisions.blade.php
+++ resources/views/templetes/laborales/provisions.blade.php
@@ -5,15 +5,18 @@
     <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
-    <meta property="og:image" content="/templetes/img/bodas/carta.jpeg">
+    <meta property="og:image" content="/templetes/img/reuniones/portada3.jpeg">
     <meta property="og:title" content="Cena de Navidad" />
     <meta property="og:url" content="provisionssystems.com/invitations/provisionsDinner" />
     <meta property="og:description" content="Porque la Navidad se trata de celebrar juntos.  Acompañanos a la cena navideña de tu empresa">
     <!-- FONTS -->
      <link href="https://fonts.googleapis.com/css?family=Dosis|Nanum+Gothic|Great+Vibes|News+Cycle&display=swap" rel="stylesheet">
-     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet"> 
+     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
     <!-- Css -->
     <link rel="stylesheet" type="text/css" href="/templetes/css/pvsDinner.css">
 
@@ -52,6 +55,7 @@
     </div>
   </main>
   <!--/Main -->
+
   <!-- -->
   <section id="textInitial" class="mt-4">
     <div class="container">
@@ -81,10 +85,66 @@
   </section>
   <!-- /-->
 
+  <!-- -->
+  <footer id="footer">
+    <div class="container ">
+      <div class="row">
+        <div class="col-12 col-md-4">
+          <label>Invitaciones:</label>
+          <ul>
+            <li>
+              <a href="#">
+                <i class="fab fa-facebook"></i>
+                facebook.com/ProVisionsSystems
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fas fa-globe-americas"></i>
+                provisionssystems.com
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fab fa-twitter-square"></i>
+                twitter.com/ProVisionsSyst
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 col-md-4 offset-md-4">
+          <label>Organizacion:</label>
+          <ul>
+            <li>
+              <a href="#">
+                <i class="fab fa-facebook"></i>
+                 facebook.com/ProVisionsSystems
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                 <i class="fas fa-globe-americas"></i>
+                provisionssystems.com
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fab fa-twitter-square"></i>
+                twitter.com/ProVisionsSyst
+              </a>
+            </li>
+          </ul>
+        </div>   
+      </div>
+    </div>
+  </footer>
+  <!-- /-->
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="/js/all.js"></script>
   </body>
 </html>
