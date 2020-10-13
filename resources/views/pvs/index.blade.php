6872e8ffcae6aeea4fe494c4c639f415b5243f07
Marisol Mondragón <marisol9430@hotmail.com>
2020-10-13 16:39:36 -0500

    prueba2

--- resources/views/pvs/index.blade.php
+++ resources/views/pvs/index.blade.php
@@ -22,25 +22,24 @@
 <header class="start-header">
   <div class="container-fluid contenido">
     <div class="container">
-      <nav class="navbar navbar-expand-lg menu">
-        <a class="navbar-brand" href="#"><img src="/img/logopvs.png" alt="" width="200"></a>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><img src="/pvs/img/logopvs.png" alt="" width="200"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-      
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item mr-4">
-              <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-              <a class="nav-link mt-1" href="asda">Aqui va otro</a>
+          <ul class="navbar-nav ml-auto d-flex align-items-center">
+            <li class="nav-item">
+              <span><i class="fab fa-facebook-square head-icon"></i>Provisions Systems</span>
+              <a class="nav-link " href="asda">Desarrollo a la medida</a>
             </li>
-            <li class="nav-item mr-4">
-              <a class="nav-link" href="#">Link</a>
-              <a class="nav-link mt-1" href="#">¿Aqui va otro texto?</a>
+            <li class="nav-item ">
+              <span><i class="fas fa-phone head-icon"></i>6677-8899</span>
+              <a class="nav-link " href="#">¿Por qué Provisions?</a>
             </li>
-            <li class="nav-item mr-4">
-                <a class="nav-link btn btn-primary" style="color:white" href="#">Contactanos</a>
-                <a class="nav-link mt-1" href="#">Solo disponible hoy</a>
+            <li class="nav-item">
+              <span class="nav-link" >&nbsp;</span>
+             <a href="" class="nav-link " id="contact-link">Contáctanos</a>
             </li>
           </ul>
         </div>
@@ -48,10 +47,7 @@
     </div>
   </div>
 </header>
-
-
- 
-  
+<script src="/pvs/js/all.js"></script>  
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
