4958ef99fb77a761835f0e71f643386fd10a2d55
Marisol Mondragón <marisol9430@hotmail.com>
2020-10-14 18:12:30 -0500

    Menu principal version 1 terminado

--- resources/views/pvs/index.blade.php
+++ resources/views/pvs/index.blade.php
@@ -12,6 +12,7 @@
 
   <meta name="keywords" content="ProVisions, provisions, ProVisions Systems, provisions systems, desarrollo,software">
 
+  <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet"> 
   <link rel="stylesheet" type="text/css" href="css/normalize.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   <link rel="stylesheet" href="/pvs/css/index.css">
@@ -22,28 +23,51 @@
 <header class="start-header">
   <div class="container-fluid contenido">
     <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#"><img src="/pvs/img/logopvs.png" alt="" width="200"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto d-flex align-items-center">
-            <li class="nav-item">
-              <span><i class="fab fa-facebook-square head-icon"></i>Provisions Systems</span>
-              <a class="nav-link " href="asda">Desarrollo a la medida</a>
-            </li>
-            <li class="nav-item ">
-              <span><i class="fas fa-phone head-icon"></i>6677-8899</span>
-              <a class="nav-link " href="#">¿Por qué Provisions?</a>
-            </li>
-            <li class="nav-item">
-              <span class="nav-link" >&nbsp;</span>
-             <a href="" class="nav-link " id="contact-link">Contáctanos</a>
-            </li>
-          </ul>
+      <div class="row">
+        <div class="col-xs-8 col-md-3">
+          <div class="site-logo">
+            <a href="">
+              <img src="/pvs/img/logopvs.png" alt="logo" width="200">
+            </a>
+          </div>
         </div>
-      </nav>
+        <div class="col-xs-4 col-md-9">
+          <nav class="site-nav">
+            <div class="contact">
+              <a href="">
+                <i class="fas fa-phone contact-icon"></i>
+              </a>
+              55 3161 7135
+              <a href="">
+                <i class="fab fa-facebook-square contact-icon"></i>
+              </a>
+              Provisions Systems
+              <button class="blue-btn">
+                Solicitar presupuesto
+              </button>
+            </div>
+            <ul class="nav-off-canvas">
+              <li>
+                <a href="">
+                  Desarrollo a la medida
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  ¿Por qué Provisions?
+                </a>
+              </li>
+              <li>
+              </li>
+              <li>
+                <a href="" id="exito">
+                  Casos de éxito
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </div>
   </div>
 </header>
