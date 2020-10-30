0b59358cf7ef6c54c16da5da8115455ad60dab22
Marisol Mondragón <marisol9430@hotmail.com>
2020-10-29 19:05:41 -0600

    seccion de alguna idea

--- resources/views/pvs/index.blade.php
+++ resources/views/pvs/index.blade.php
@@ -210,6 +210,23 @@
     </div>
   </div>
 </section>
+<section class="have-idea shadow-sm p-3 mb-5 bg-white">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 class="text-center work-h1">¿Tienes algúna idea?</h1>
+        <hr style="width: 25%; background-color:  #a5d936; height: 3px; margin-bottom: 30px;">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h3 class=" work-h3">Acercate a nosotros, haremos realidad esa idea que tienes en mente,
+          con nuestro equipo de trabajo.</h3>
+          <a href="" class="nav-link green-btn hvr-push">Contáctanos ahora</a>
+      </div>
+    </div>
+  </div>
+</section>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
