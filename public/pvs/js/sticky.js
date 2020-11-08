a629261ca797bdadc3d685a4bb0395447dd0a397
JACustodioD <JACustodioD@hotmail.com>
2020-11-08 16:54:29 -0600

    fixed menu

--- public/pvs/js/sticky.js
+++ public/pvs/js/sticky.js
@@ -2,6 +2,7 @@
 
     if($(window).scrollTop() > 10) {
       $(".start-header").attr("id",'sticky-menu');
+      $(".start-header").addClass('menu-fixed');
     } else {
       $(".start-header").removeAttr("id");
     }
