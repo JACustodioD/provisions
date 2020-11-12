f5d4cf72d8eb0d5aba7a285e207c78e0d91dd9e0
JACustodioD <JACustodioD@hotmail.com>
2020-11-12 17:00:03 -0600

    solucionando la torpeza de sunset

--- resources/views/pvs/index.blade.php
+++ resources/views/pvs/index.blade.php
@@ -146,48 +146,4 @@
   </div>
 </div>
 </section>
-<section class="footer-cont shadow-sm p-3">
-  <footer class="container">
-    <div class="row">
-      <div class="col-md-3">
-        <img src="/pvs/img/logopvs.png" alt="" width="150">
-      </div>
-      <div class="col-md-9 mt-3 footer-text">
-        <ul class="d-flex justify-content-around foot-ul">
-          <li>
-            <a href="#"><i class="fas fa-circle mr-1" style="color: #0064de;"></i>Desarrollo a la medida </a>
-          </li>
-          <li>
-            <a href="#"><i class="fas fa-circle mr-1" style="color: #0064de;"></i>Nosotros</a>
-          </li>
-          <li >
-            <a href="#"><i class="fas fa-circle mr-1" style="color: #0064de;"></i>Contácto</a>
-          </li>
-        </ul>
-      </div>
-     </div>
-    <div class="row d-flex justify-content-end">
-    
-      <div class="col-md-5 col-lg-3">
-         <small class="p-2"><i class="fas fa-kaaba" style="color: #a5d936;"></i> Provisions Systems S.A. de C.V.</small>
-      </div>
-      <div class="col-md-4 col-lg-2">
-         <small class="p-2"><a class="text-menu" href="https://www.facebook.com/ProVisionsSystems"><i class="fab fa-facebook-square icon-menu" style="color: #a5d936;"></i> <span class="text-head-res">Provisions Systems</span> </a></small>
-      </div>
-    </div>
-  </footer>
-</section>
-<div class="">
-<span class="botoncito"><i class="fas fa-angle-double-up"></i></span>
-</div>
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script src="/pvs/js/all.js"></script>  
-<script src="/pvs/js/efectos.js"></script> 
-<script src="/pvs/js/jquery.js"></script> 
-<script src="/pvs/js/boton.js"></script>
-<script src="/pvs/js/sticky.js"></script>  
-</body>
-</html>
+@endsection
