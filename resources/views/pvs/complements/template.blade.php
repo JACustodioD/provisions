b1916fc7f800c142bf38ecd59489819dccfbbb8e
JACustodioD <JACustodioD@hotmail.com>
2020-11-13 09:53:44 -0600

    agregando analytics

--- resources/views/pvs/complements/template.blade.php
+++ resources/views/pvs/complements/template.blade.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-180797871-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-180797871-1');
+</script>
+
   
   <title>@yield('title')</title>
   
