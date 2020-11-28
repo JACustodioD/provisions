3ff865e8852cd981080c1457b8a5875011770672
JACustodioD <JACustodioD@hotmail.com>
2020-11-27 18:30:51 -0600

    correcciones en links

--- resources/views/pvs/complements/footer.blade.php
+++ resources/views/pvs/complements/footer.blade.php
@@ -28,7 +28,7 @@
                   <a href="/nosotros/#about"  class="sub-text-foot"><i class="fas fa-circle mr-1" style="color: #a5d936; font-size:0.7em;"></i>Misión, visión y valores</a> 
                 </li>
                 <li>
-                  <a href="/exito"  class="sub-text-foot"><i class="fas fa-circle mr-1" style="color: #a5d936; font-size:0.7em;"></i>Casos de éxito</a> 
+                  <a href="/casos-de-exito"  class="sub-text-foot"><i class="fas fa-circle mr-1" style="color: #a5d936; font-size:0.7em;"></i>Casos de éxito</a> 
                 </li>
               </ul>
             </li>
@@ -37,13 +37,13 @@
                 <h1  class="footer-text"> Contácto </h1>
               <ul>
                 <li>
-                  <a href="https://www.facebook.com/ProVisionsSystems" class="sub-text-foot"><i class="fab fa-facebook mr-1" style="color: #a5d936; font-size:1em;"></i>Provisions Systems</a> 
+                  <a href="https://www.facebook.com/ProVisionsSystems" class="sub-text-foot"><i class="fab fa-facebook mr-1" style="color: #a5d936; font-size:1em;"></i>ProVisions Systems</a> 
                 </li>
                 <li>
                   <a href="https://api.whatsapp.com/send?phone=525531617135&text=Hola, me podrían dar más información. Tengo en mente un proyecto."  class="sub-text-foot"><i class="fab fa-whatsapp mr-1" style="color: #a5d936; font-size:1em;"></i>55 3161 7135</a> 
                 </li>
                 <li>
-                  <a href="https://www.instagram.com/ProVisionSystems"  class="sub-text-foot"><i class="fab fa-instagram mr-1" style="color: #a5d936; font-size:1em;"></i>Provisionsystems</a> 
+                  <a href="https://www.instagram.com/ProVisionSystems"  class="sub-text-foot"><i class="fab fa-instagram mr-1" style="color: #a5d936; font-size:1em;"></i>ProVisions Systems</a> 
                 </li>
               </ul>
             </li>
