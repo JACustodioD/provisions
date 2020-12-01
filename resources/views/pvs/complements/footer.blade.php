fb2438195c98759df4ee53ca626d321b3ac20e5a
Marisol Mondragón <marisol9430@hotmail.com>
2020-11-30 19:22:53 -0600

    Agregando link a pvs en footer

--- resources/views/pvs/complements/footer.blade.php
+++ resources/views/pvs/complements/footer.blade.php
@@ -53,7 +53,7 @@
        </div>
       <div class="row text-center">
          <div class="col-md-12">
-           <small class="p-2"><i class="fas fa-kaaba" style="color:#0064de;"></i> Designed By: <b>ProVisions Systems</b></small>
+           <small class="p-2"><i class="fas fa-kaaba" style="color:#0064de;"></i> Designed By: <a href="/"> ProVisions Systems </a></small>
         </div>
     </footer>
 </section>
